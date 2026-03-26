c067b0d797cf495c65bda819a0017d156871ac12
Leidy Yohana Giraldo <yohanagiraldo1902@gmail.com>
2026-03-25 20:09:40 -0500

    -Se crea el DTO de Actividad para separar la entidad de la capa de servicio. - Se reemplaza @Autowired por @RequiredArgsConstructor para inyección de   dependencias. -Se ajusta el método de creación de actividad para evitar duplicados por ID y prevenir sobrescritura de la información.

--- src/main/java/com/agroKont/kontAgro/service/contracts/IActividadService.java
+++ src/main/java/com/agroKont/kontAgro/service/contracts/IActividadService.java
@@ -1,15 +1,16 @@
 package com.agroKont.kontAgro.service.contracts;
 
+import com.agroKont.kontAgro.dto.Class.ActividadDTO;
 import com.agroKont.kontAgro.entities.Actividad;
 import org.springframework.http.ResponseEntity;
 
 public interface IActividadService {
 
-    ResponseEntity<Actividad> crearActividad(Actividad actividad);
+    ResponseEntity<?> crearActividad(ActividadDTO actividad);
 
     ResponseEntity<?> consultarActividad(Integer id);
 
-    ResponseEntity<Actividad> actualizarActividad(Actividad actividad);
+    ResponseEntity<ActividadDTO> actualizarActividad(ActividadDTO actividad);
 
     ResponseEntity<?> consultarActividad();
 }
